cd9568aa403c2a635d8b6a20452f07ab431080e2
Christian Page <christian.page@cerfacs.fr>
2020-08-14 11:51:23 +0200

    Removed underscore in python package naming.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='cyclone_tracking_products',  # Required
+    name='cyclonetrackprod',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
